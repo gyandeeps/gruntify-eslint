61e9fcae6bde54f8c682239e1f27ad2b163a2689
gyandeeps <gyandeeps@gmail.com>
2014-07-08 16:26:48 -0500

    Adding the silent property and also support different definitions

--- tasks/eslint.js
+++ tasks/eslint.js
@@ -3,15 +3,38 @@ var path = require("path");
 module.exports = function(grunt){
     grunt.registerMultiTask("eslint", "Validate files with ESLint", function(){
         var eslint = require("eslint").cli;
-        var options = this.options();
+        var response;
+        var silent;
+        var options = this.options({
+            "silent": false //If you want the grunt task to fail or just be success
+        });
 
-        if(this.filesSrc.length === 0){
-            return console.log("No Files specified");
+        //This is if you are not using multiple tasks
+        if(this.target === "target"){
+            if(this.filesSrc.length === 0){
+                return console.log("No Files specified");
+            }
+            options._ = this.filesSrc;
+        }
+        else{ //This for multiple tasks
+            if(this.data.target.length === 0){
+                return console.log("No Files specified");
+            }
+            options._ = this.data.target;
         }
 
-        options._ = this.filesSrc;
-        options.config = options.config ? path.resolve(options.config) : "";
+        silent = options.silent;
+        delete options.silent;
 
-        return eslint.execute(options) === 0;
+        options.config = options.config ? path.resolve(options.config) : "";
+        response = eslint.execute(options);
+
+        if(silent){
+            return true;
+        }
+        else{
+            return response === 0;
+        }
+
     });
 };
