870d4a3d719a137d96b296478cb013a2c42449b0
Gyandeep Singh <gyandeeps@gmail.com>
2015-07-27 17:54:15 -0500

    Add: Automate the release process

---
+++ Makefile.js
@@ -0,0 +1,30 @@
+"use strict";
+require("shelljs/make");
+
+function release(type){
+    exec("echo Bumping up the version");
+    exec("npm version " + type);
+
+    exec("echo Commiting master with tags");
+    exec("git add package.json");
+    exec("git commit --amend --no-edit");
+    exec("git push origin master --tags");
+
+    exec("echo Publish on NPM");
+    exec("npm publish");
+
+    exec("echo Operation done.");
+}
+
+
+target.patch = function(){
+    release("patch");
+};
+
+target.minor = function(){
+    release("minor");
+};
+
+target.major = function(){
+    release("major");
+};
